32ae6717cb3308e20cf56a1c03a64545ad628bf1
John Pasula <ackis81@gmail.com>
2018-04-30 08:30:59 -0600

    Make lazymans script to copy things over to /etc/update-motd.d/

---
+++ movescript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# If it's not root, the script restarts itself through sudo.
+[[ $UID = 0 ]] || exec sudo "$0"
+
+cp ./01-banner /etc/update-motd.d/
+cp ./02-last-login /etc/update-motd.d/
+cp ./03-uptime /etc/update-motd.d/
+cp ./04-load-average /etc/update-motd.d/
+cp ./05-memory /etc/update-motd.d/
+cp ./06-logins /etc/update-motd.d/
+cp ./07-processes /etc/update-motd.d/
+cp ./08-plex-stats /etc/update-motd.d/
+cp ./09-transmission-stats /etc/update-motd.d/
+cp ./10-sabnzbd-stats /etc/update-motd.d/
+cp ./11-process-check /etc/update-motd.d/

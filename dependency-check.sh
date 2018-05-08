e4b181a05b8e48078d09fefc75b3340171d3470a
Jonathan Cremin <jonathan@crem.in>
2018-05-08 20:02:38 +0100

    Check for transmission-remote-cli

--- dependency-check.sh
+++ dependency-check.sh
@@ -9,3 +9,8 @@ if ! [ -e /usr/bin/bc ]; then
 	echo "Warning: /usr/bin/bc not found, please run 'sudo apt-get install bc'"
 	exit 1
 fi
+
+if ! [ -e /usr/bin/transmission-remote-cli ]; then
+	echo "Warning: /usr/bin/transmission-remote-cli not found, please run 'sudo apt-get install transmission-remote-cli'"
+	exit 1
+fi

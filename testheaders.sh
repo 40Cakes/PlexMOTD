e836957a26fb00c1dc40292e3ac8c0bc56fb1fec
John Pasula <ackis81@gmail.com>
2018-05-09 10:31:09 -0600

    Make testheaders use that amazing concept of an array as well.

--- testheaders.sh
+++ testheaders.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-./01-banner
-./02-last-login
-./03-uptime
-./04-load-average
-./05-memory
-./06-logins
-./07-processes
-./08-plex-stats
-./09-transmission-stats
-./10-sabnzbd-stats
-./11-process-check
+# If it's not root, the script restarts itself through sudo.
+# We need to run as root because /etc/update-motd.d/config.conf is rw for root only
+[[ $UID = 0 ]] || exec sudo "$0"
+
+FILELIST=(
+	"01-banner"
+	"02-last-login"
+	"03-uptime"
+	"04-load-average"
+	"05-memory"
+	"06-logins"
+	"07-processes"
+	"08-plex-stats"
+	"09-transmission-stats"
+	"10-sabnzbd-stats"
+	"11-process-check"
+)
+
+
+for index in "${!FILELIST[@]}"; do
+	"./${FILELIST[index]}"
+done
+
+for index in "${!FILELIST[@]}"; do
+	"/etc/update-motd.d/${FILELIST[index]}"
+done

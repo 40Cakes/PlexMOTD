56767b255afccd6b6388b95f0fa40abf85252ab2
John Pasula <ackis81@gmail.com>
2018-05-09 09:05:49 -0600

    Fix an error with copying.  And prevent services from being copied with the new changes made to the process checks script.

--- movescript.sh
+++ movescript.sh
@@ -2,9 +2,10 @@
 
 # If it's not root, the script restarts itself through sudo.
 [[ $UID = 0 ]] || exec sudo "$0"
-cp ./01-banner ./colors.txt /etc/update-motd.d/
-cp ./01-banner ./config.conf /etc/update-motd.d/
-cp ./01-banner ./services /etc/update-motd.d/
+
+cp ./colors.txt /etc/update-motd.d/
+cp ./config.conf /etc/update-motd.d/
+#cp ./01-banner ./services /etc/update-motd.d/
 cp ./01-banner /etc/update-motd.d/
 cp ./02-last-login /etc/update-motd.d/
 cp ./03-uptime /etc/update-motd.d/
